30fcfae6b060c76db93eed25f29afdfce4d8b8c9
Arthur Goldman <arthur@goldman-tribe.org>
2020-01-24 16:34:15 -0600

    quash an asdf warning and keep .lisp files out of the source tree

--- main.lisp
+++ main.lisp
@@ -2,8 +2,10 @@
     (:use :cl)
   (:nicknames :asdf-org-babel-tangle)
   (:import-from :asdf
-   :cl-source-file :operation :input-files :output-files
-   :component-name :perform :component-depends-on :compile-op :load-op)
+   :cl-source-file :sideway-operation :upward-operation :input-files :output-files
+   :component-pathname :component-name :perform :component-depends-on
+                :compile-op :load-op
+                :make-operation)
   (:import-from :uiop :run-program)
   (:export :org-source-file :tangle-op))
 (cl:in-package :asdf-org-babel-tangle/main)
@@ -12,26 +14,32 @@
   ()
   (:documentation "component class for an org-mode literate text file that can tangled into a common lisp source file"))
 
-(defclass tangle-op (operation)
+(defclass tangle-op (sideway-operation upward-operation)
   ()
   (:documentation "use org-babel-tangle to convert an org-source-file into a cl-source-file"))
 
+(defmethod input-files ((op compile-op) (file org-source-file))
+  (output-files (make-operation 'tangle-op) file))
+
 (defmethod input-files ((op tangle-op) (file org-source-file))
-  (list (concatenate 'string (component-name file) ".org")))
+  (list (merge-pathnames (make-pathname :type "org")
+                         (component-pathname file))))
 
 (defmethod output-files ((op tangle-op) (file org-source-file))
-  (list (concatenate 'string (component-name file) ".lisp")))
+  (list (component-pathname file)))
+
+(defun tangle (source-path output-path)
+  (let ((command (format nil
+                         "emacs -batch ~s --eval '(org-babel-tangle nil ~s ~s)'"
+                         (namestring source-path)
+                         (namestring output-path)
+                         "lisp")))
+    (run-program command)))
 
 (defmethod perform ((op tangle-op) (file org-source-file))
   (let* ((input-file (first (input-files op file)))
-         (output-file (first (output-files op file)))
-         (babel-tangle-arg-source-lang "lisp")
-         (command (format nil
-                          "emacs -batch ~s --eval '(require 'ob-tangle) (org-babel-tangle nil ~s ~s)"
-                          input-file
-                          output-file
-                          babel-tangle-arg-source-lang)))
-    (run-program command)))
+         (output-file (first (output-files op file))))
+    (tangle input-file output-file)))
 
 (defmethod component-depends-on ((op compile-op) (file org-source-file))
   `((tangle-op ,(component-name file))
